1fd61f8081c43ee95b4eaf1e3cdf63ad08eba8e0
jdegand <70610011+jdegand@users.noreply.github.com>
2024-10-03 22:38:29 -0400

    fix: update dependency arrays in Channel & more

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,14 +1,13 @@
-import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
-import { useNavigate, useLocation, Link } from "react-router-dom";
+import { useParams, useNavigate, useLocation, Link } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import Messages from './Messages';
 import LoggedInUser from "./LoggedInUser";
 
 const Channel = (props) => {
 
-    const {channelId} = useParams();
+    const { channelId } = useParams();
     const axiosPrivate = useAxiosPrivate();
     const navigate = useNavigate();
     const location = useLocation();
@@ -33,7 +32,7 @@ const Channel = (props) => {
                 const response = await axiosPrivate.get(`/channels/${channelId}`, {
                     signal: controller.signal
                 });
-    
+
                 isMounted && setChannel(response.data);
             } catch (err) {
                 console.error(err);
@@ -47,15 +46,15 @@ const Channel = (props) => {
             isMounted = false;
             controller.abort();
         }
-    }, []) // channel  - updates when members array changes but causes many requests to be fired
+    }, [axiosPrivate, channelId, location, navigate]) // channel  - updates when members array changes but causes many requests to be fired
 
-    
+
     // Adding a cleanup to this - userInfo profile undefined
     useEffect(() => {
         const getChannel = async () => {
             try {
                 const response = await axiosPrivate.get(`/channels/${channelId}`);
-    
+
                 setChannel(response.data);
             } catch (err) {
                 console.error(err);
@@ -63,87 +62,67 @@ const Channel = (props) => {
         }
         getChannel();
 
-    }, [refreshChannel])
+    }, [refreshChannel, axiosPrivate, channelId])
 
     // add user to channel on first navigation to channel
     // need to use axios put to channel
-    
-    useEffect(()=> {
+
+    useEffect(() => {
         const updateChannel = async () => {
             try {
-                const response = await axiosPrivate.get(`/channels/channel/${channelId}/user/${auth.user}/userId/${auth.id}`);
-    
-                //console.log('updateChannel',response)
+                await axiosPrivate.get(`/channels/channel/${channelId}/user/${auth.user}/userId/${auth.id}`);
             } catch (err) {
                 console.error(err);
             }
         }
         updateChannel();
         setRefreshChannel(prev => !prev) // move this into try block ?
-    }, [])
-    
-
-    // replaced this useEffect by using populate when getting the channel to display the messages
-    /*
-    useEffect(()=> {
-        const getChannelMessages = async () => {
-            try {
-                const response = await axiosPrivate.get(`/channels/${channelId}/messages`);
-    
-                console.log('getChannelMessages', response)
-                setMessages(response.data);
-            } catch (err) {
-                console.error(err);
-            }
-        }
-        getChannelMessages();
-    }, [])
-    */
+    }, [auth.id, auth.user, axiosPrivate, channelId])
 
     return (
         <div className="flex">
             <div>
-            <section className={props.mobileToggle ? "channels-section channels-section-mobile" :"channels-section mobile-hidden"}>
-            <div>
-                <div className="flex align-center">
-                    <div className="back-link">
-                        <Link to="/" aria-label="Go Back to Channels">
-                            <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
-                            </svg>
-                        </Link>
-                    </div>
-                    <h1 className="white-text">All Channels</h1>
-                </div>
-                <h2 className="ml-10 mb-20">
-                    {channel?.name}
-                </h2>
-                <h3 className="ml-10 mb-20">
-                    {channel?.description}
-                </h3>
-                <div className="channel-members-div">
-                {
-                    channel?.members.map(member => {
-                        return (
-                            <div className="channel-member-div" key={member._id}>
-                                <span><img className="channel-member-div-img" src={member.picture} alt="" /></span>
-                                <span className="ml-10">{member.username}</span>
+                <section className={props.mobileToggle ? "channels-section channels-section-mobile" : "channels-section mobile-hidden"}>
+                    <div>
+                        <div className="flex align-center">
+                            <div className="back-link">
+                                <Link to="/" aria-label="Go Back to Channels">
+                                    <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                        <path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
+                                    </svg>
+                                </Link>
                             </div>
-                        )
-                    })
-                }
-                </div>
-            </div>
-            </section>
-            <LoggedInUser mobileToggle={props.mobileToggle} channel={true} />
+                            <h1 className="white-text">All Channels</h1>
+                        </div>
+                        <h2 className="ml-10 mb-20">
+                            {channel?.name}
+                        </h2>
+                        <h3 className="ml-10 mb-20">
+                            {channel?.description}
+                        </h3>
+                        <div className="channel-members-div">
+                            {
+                                channel?.members.map(member => {
+                                    return (
+                                        <div className="channel-member-div" key={member._id}>
+                                            <span><img className="channel-member-div-img" src={member.picture} alt="" /></span>
+                                            <span className="ml-10">{member.username}</span>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                </section>
+                <LoggedInUser mobileToggle={props.mobileToggle} channel={true} />
             </div>
             <div>
-            <Messages 
-                handleMobileToggle={props.handleMobileToggle} 
-                mobileChannelToggle={props.mobileChannelToggle} 
-                handleMobileChannelToggle={props.handleMobileChannelToggle} 
-                channel={true} 
-            />
+                <Messages
+                    handleMobileToggle={props.handleMobileToggle}
+                    mobileChannelToggle={props.mobileChannelToggle}
+                    handleMobileChannelToggle={props.handleMobileChannelToggle}
+                    channel={true}
+                />
             </div>
         </div>
     )
